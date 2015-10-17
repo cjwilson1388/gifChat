8d98aa1d644e7d5bf8829afadcffa5cdcc53e6f1
CJ Wilson <cjwilson1388@gmail.com>
2015-10-17 12:03:29 -0600

    Inital Push for project

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+var app = angular.module('giphChat');
+
+app.controller('mainCntl', ['', function(){
+	
+}])
---
+++ js/service.js
@@ -0,0 +1,6 @@
+var app = angular.module('giphChat');
+
+app.factory('giphService', function(){
+	
+		
+})
